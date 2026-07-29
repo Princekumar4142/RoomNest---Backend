2dce330a1182ea5a46f9fb2a7d9339079237a67d
Prince kumar <princebth1988@gmail.com>
2026-07-29 19:00:03 +0530

    git update

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "https://roomnest-backend-pnf5.onrender.com/api",
     credentials: true,
   })
 );
